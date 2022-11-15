1e95141d5d020166f51b3e0fa7142a5a577b017d
Ankit S <ankit@designstring.com>
2022-11-15 16:47:43 +0530

    fixed bridged api error response

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def get_exchange_type(taker_address:str,sell_token:str,buy_token:str,sell_
     print("running in functions ..")
     response = {}
     # required params
-    exchange_types = ["1inch","0x", "KyberSwap", "Paraswap"]  # list of token exchance service providers
+    exchange_types = ["1inch","0x","KyberSwap", "Paraswap"]  # list of token exchance service providers
     base_url = "https://v2-dev.unifront.io/v2/exchange"
     for exchange_type in (exchange_types):
         main_url = f"{base_url}/polygon/swap/?exchange_type={exchange_type}&taker_address={taker_address}&sell_token={sell_token}&buy_token={buy_token}&sell_amount={sell_amount}&slippage={slippage}"
@@ -31,16 +31,15 @@ async def get_exchange_type(taker_address:str,sell_token:str,buy_token:str,sell_
 
 # Asynchronized bridge token API
 @app.get("/bridge/token")
-async def get_bridge_provider(from_chain:str,to_chain:str,from_token:str,to_token:str,from_amount:str,
-                            from_address:str, to_address:str, request : Request):
+# async def get_bridge_provider(from_chain:str,to_chain:str,from_token:str,to_token:str,from_amount:str,from_address:str, to_address:str, request : Request):
+async def get_bridge_provider(from_chain:str,to_chain:str,from_token:str,to_token:str,from_amount:str,from_address:str,to_address:str,request:Request):
     response = {}
-    bridge_providers = ["Router","LI.FI", "XY", "Socket", "deBridge", "Range"]  # list of bridge token service providers
-    base_url = "https://v2-dev.unifront.io/v2/bridge/transaction"
+    bridge_providers = ["router","LI.FI", "XY", "Socket", "deBridge", "Range"]  # list of bridge token service providers
+    base_url = "https://v2-dev.unifront.io/v2/bridge/quote"
     for bridge_provider in (bridge_providers):
-        main_url = f"""{base_url}?from_chain={from_chain}&to_chain={to_chain}&from_token={from_token}&to_token={to_token}
-                        &from_amount={from_amount}&from_address={from_address}&to_address={to_address}&bridge_provider={bridge_provider}"""
-        result =  session.get(main_url)
-        data = result.json()
+        main_url = f"{base_url}?from_chain={from_chain}&to_chain={to_chain}&from_token={from_token}&to_token={to_token}&from_amount={from_amount}&from_address={from_address}&to_address={to_address}&bridge_provider={bridge_provider}"
+        final_response =  session.get(main_url)
+        data = final_response.json()
         if data.get('status_code') == 200:
             # print("required bridge_provider", bridge_provider)
             response['data'] = {"bridge_provider" : bridge_provider}
